80d52301fe4d059109bb83e3811d9f32e23d133b
Sniddunc <sniddunc2@gmail.com>
2021-02-01 14:21:36 -0500

    Users Table Creation + Project Dialect Settings

---
+++ backend/internal/storage/mysql/mysql.go
@@ -0,0 +1,39 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+func Setup(db *sql.DB) error {
+	ctx := context.Background()
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("Could not create transaction. Error: %v", err)
+	}
+
+	// Create users table
+	if _, err := tx.Exec(`
+		CREATE TABLE IF NOT EXISTS Users(
+			UserID INT NOT NULL AUTO_INCREMENT,
+			Username VARCHAR(32) UNIQUE NOT NULL,
+			Email VARCHAR(254) UNIQUE NOT NULL,
+			Password BINARY(60) NOT NULL,
+			AccessLevel INT DEFAULT 0,
+			Activated BOOLEAN DEFAULT TRUE,
+			NeedsPasswordChange BOOLEAN DEFAULT TRUE,
+			
+			PRIMARY KEY (UserID)
+		);
+	`); err != nil {
+		if err = tx.Rollback(); err != nil {
+			return err
+		}
+
+		return fmt.Errorf("could not create Users table. Error: %v", err)
+	}
+
+	return tx.Commit()
+}
