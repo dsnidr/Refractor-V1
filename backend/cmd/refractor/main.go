0e5a5ebcba708e06b60b33b6b32c181e5f7c608b
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-02-01 16:32:48 -0500

    Backend Initial User Creation

--- backend/cmd/refractor/main.go
+++ backend/cmd/refractor/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"github.com/sniddunc/refractor/internal/params"
 	"github.com/sniddunc/refractor/internal/storage/mysql"
+	"github.com/sniddunc/refractor/internal/user"
 	"github.com/sniddunc/refractor/pkg/env"
 	logger "github.com/sniddunc/refractor/pkg/log"
+	"github.com/sniddunc/refractor/refractor"
 	"log"
 	"os"
 )
 
 func main() {
 	if err := godotenv.Load("./.env"); err == nil {
-		log.Println("Environment variables loaded from .env file")
+		fmt.Println("Environment variables loaded from .env file")
 	}
 
 	if err := env.RequireEnv("DB_URI").
@@ -30,11 +34,25 @@ func main() {
 	}
 
 	// Setup database
-	_, err = setupDatabase(os.Getenv("DB_URI"))
+	db, err := setupDatabase(os.Getenv("DB_URI"))
 	if err != nil {
 		log.Fatalf("Could not setup database. Error: %v", err)
 	}
 
+	// Set up application components
+	userRepo := mysql.NewUserRepository(db)
+	userService := user.NewUserService(userRepo, loggerInst)
+
+	// Set up initial user if no users currently exist
+	if count := userRepo.GetCount(); count == 0 {
+		if err := setupInitialUser(userService); err != nil {
+			log.Fatalf("Could not create initial user. Error: %v", err)
+		}
+
+		loggerInst.Info("Initial user created from environment variables")
+	}
+
+	// Done. Begin serving.
 	loggerInst.Info("Refractor startup complete!")
 }
 
@@ -54,4 +72,34 @@ func setupDatabase(connString string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func setupInitialUser(userService refractor.UserService) error {
+	if err := env.RequireEnv("INITIAL_USER_USERNAME").
+		RequireEnv("INITIAL_USER_EMAIL").
+		RequireEnv("INITIAL_USER_PASSWORD").
+		GetError(); err != nil {
+		return err
+	}
+
+	body := params.CreateUserParams{
+		Email:           os.Getenv("INITIAL_USER_EMAIL"),
+		Username:        os.Getenv("INITIAL_USER_USERNAME"),
+		Password:        os.Getenv("INITIAL_USER_PASSWORD"),
+		PasswordConfirm: os.Getenv("INITIAL_USER_PASSWORD"),
+	}
+
+	if ok, errors := body.Validate(); !ok {
+		return fmt.Errorf("validation errors occurred: %v", errors)
+	}
+
+	// Create user
+	_, res := userService.CreateUser(body)
+	if !res.Success {
+		return fmt.Errorf("could not create initial user: %s", res.Message)
+	}
+
+	// TODO: Update user to make them a super-admin
+
+	return nil
 }
