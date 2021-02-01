b1afaa88d798409fbe2cd374cbdfc2cf2a730f57
Sniddunc <sniddunc2@gmail.com>
2021-02-01 12:47:06 -0500

    Backend Auth types and Dependencies

---
+++ backend/pkg/config/locale.go
@@ -0,0 +1,10 @@
+package config
+
+var (
+	MessageInternalError      = "Something went wrong. Please try again later"
+	MessageInvalidCredentials = "Invalid credentials were provided"
+	MessageDeactivatedAccount = "That account is deactivated"
+	MessageUnableRefreshCreds = "We were unable to refresh your credentials. Please log in again"
+	MessageInvalidIDProvided  = "The provided ID is invalid"
+	MessageServerNotFound     = "Server not found"
+)
