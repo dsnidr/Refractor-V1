c0fd8b1cb0a9b3f3b3d9e69fa27cedfa49c6eaab
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-02-01 12:50:37 -0500

    Backend User Types

---
+++ backend/refractor/user.go
@@ -0,0 +1,32 @@
+package refractor
+
+import "github.com/labstack/echo/v4"
+
+type User struct {
+	UserID              int64  `json:"id"`
+	Email               string `json:"email"`
+	Username            string `json:"username"`
+	Password            string `json:"-"`
+	AccessLevel         int    `json:"accessLevel"`
+	Activated           bool   `json:"activated"`
+	NeedsPasswordChange bool   `json:"needsPasswordChange"`
+}
+
+type UserRepository interface {
+	Create(user *User) error
+	FindByID(id int64) (*User, error)
+	Exists(args FindArgs) (bool, error)
+	FindOne(args FindArgs) (*User, error)
+	FindMany(args FindArgs) ([]*User, error)
+	Update(id int64, args UpdateArgs) (*User, error)
+	FindAll() ([]*User, error)
+	GetCount() int
+}
+
+type UserService interface {
+	GetUserInfo(id int64) (*User, *ServiceResponse)
+}
+
+type UserHandler interface {
+	GetOwnUserInfo(c echo.Context) error
+}
