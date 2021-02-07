3e78ed3d36000460407a030fa9f8a8230c27e814
Sniddunc <sniddunc2@gmail.com>
2021-02-07 14:40:36 -0500

    Frontend Theme Definitions

---
+++ frontend/src/themes/index.js
@@ -0,0 +1,14 @@
+import dark from './dark';
+import light from './light';
+
+const theme = {
+	dark: {
+		...dark,
+	},
+
+	light: {
+		...light,
+	},
+};
+
+export default theme;
---
+++ frontend/src/themes/light.js
@@ -0,0 +1,37 @@
+export default {
+	// Main colors
+	colorPrimary: '#00A8E8',
+	colorPrimaryLight: '#12beff',
+	colorPrimaryDark: '#0074a1',
+
+	// Background colors
+	colorBackground: '#ffffff',
+	colorBackgroundDark: '#00A8E8',
+	colorBackgroundAlt: '#202a2e',
+
+	// Border radius
+	borderRadiusNormal: '3px',
+
+	// Shadow definitions
+	boxShadowPrimary: {
+		'-webkit-box-shadow': '0px 0px 40px -20px rgba(0, 0, 0, 1)',
+		'-moz-box-shadow': '0px 0px 40px -20px rgba(0, 0, 0, 1)',
+		'box-shadow': '0px 0px 40px -20px rgba(0, 0, 0, 1)',
+	},
+
+	///////////////////////////
+	// Component Options
+	inputs: {
+		fillInBackground: true,
+	},
+
+	///////////////////////////
+	// Dashboard Options
+	dashboard: {
+		wrapperPadding: '0',
+		gridGap: '0',
+		contentPadding: '3rem',
+		borderRadius: false,
+		topBarShadow: true,
+	},
+};
