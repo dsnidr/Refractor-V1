2abe27227e71b6ad6e7a579196360777fcb18606
Sniddunc <sniddunc2@gmail.com>
2021-02-07 14:54:23 -0500

    Frontend Theme Switching

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,7 +1,9 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import userReducer from "./user/userReducer";
+import themeReducer from './theme/themeReducer';
+import userReducer from './user/userReducer';
 
 export default combineReducers({
-    user: userReducer,
-})
+	theme: themeReducer,
+	user: userReducer,
+});
