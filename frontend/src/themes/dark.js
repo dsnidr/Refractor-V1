5270f36883944f3039528b1e12b0c45765566919
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-02-07 14:40:36 -0500

    Frontend Theme Definitions

---
+++ frontend/src/themes/dark.js
@@ -0,0 +1,30 @@
+export default {
+	// Main colors
+	colorPrimary: '#00A8E8',
+	colorPrimaryLight: '#00b9ff',
+	colorPrimaryDark: '#0074a1',
+
+	// Background colors
+	colorBackground: '#00171F',
+	colorBackgroundDark: '#00131a',
+	colorBackgroundAlt: '#00171F',
+
+	// Border radius
+	borderRadiusNormal: '3px',
+
+	///////////////////////////
+	// Component Options
+	inputs: {
+		fillInBackground: false,
+	},
+
+	///////////////////////////
+	// Dashboard Options
+	dashboard: {
+		wrapperPadding: '2rem',
+		gridGap: '2rem',
+		contentPadding: '0',
+		borderRadius: true,
+		topBarShadow: false,
+	},
+};
