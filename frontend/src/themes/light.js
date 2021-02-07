9269ed68e716ecdd055ae811c842f7b7f6f6ddef
Sniddunc <sniddunc2@gmail.com>
2021-02-07 15:52:37 -0500

    Frontend Text Input Component
    Brought in and adapted from one of my previous projects

--- frontend/src/themes/light.js
+++ frontend/src/themes/light.js
@@ -3,6 +3,12 @@ export default {
 	colorPrimary: '#00A8E8',
 	colorPrimaryLight: '#12beff',
 	colorPrimaryDark: '#0074a1',
+	colorDanger: '#FF0035',
+	colorAlert: '#FBB13C',
+	colorWarning: '#FFFD82',
+	colorSuccess: '#45CB85',
+	colorAccent: '#edf6f7',
+	colorDisabled: '#a2b2b8',
 
 	// Background colors
 	colorBackground: '#ffffff',
