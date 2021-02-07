69ef25ef2f2fa3fbdf1fcdc7a1224162445852e3
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-02-07 16:27:15 -0500

    Frontend Authentication Flow

--- frontend/src/redux/user/userActions.js
+++ frontend/src/redux/user/userActions.js
@@ -1,11 +1,17 @@
+import { GET_USER_INFO, SET_CURRENT_USER } from './constants';
+
 export const LOG_IN = 'LOG_IN';
 export const logIn = (credentials) => ({
 	type: LOG_IN,
 	payload: credentials,
 });
 
-export const SET_USER = 'SET_USER';
 export const setUser = (data) => ({
-	type: SET_USER,
+	type: SET_CURRENT_USER,
 	payload: data,
+});
+
+export const getUserInfo = () => ({
+	type: GET_USER_INFO,
+	payload: null,
 });
