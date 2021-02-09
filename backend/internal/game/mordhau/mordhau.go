dac2b4bf8bc92b03d1aecc255b2ff97b276d253b
Sniddunc <sniddunc2@gmail.com>
2021-02-09 13:03:54 -0500

    Additional Game Interface Methods

--- backend/internal/game/mordhau/mordhau.go
+++ backend/internal/game/mordhau/mordhau.go
@@ -3,17 +3,29 @@ package mordhau
 import (
 	"fmt"
 	"github.com/sniddunc/refractor/refractor"
+	"time"
 )
 
 type mordhau struct {
+	config *refractor.GameConfig
 }
 
 func NewMordhauGame() refractor.Game {
-	return &mordhau{}
+	return &mordhau{
+		config: &refractor.GameConfig{
+			UseRCON:           true,
+			SendAlivePing:     true,
+			AlivePingInterval: time.Second * 30,
+		},
+	}
 }
 
 func (g *mordhau) GetName() string {
 	return "Mordhau"
+}
+
+func (g *mordhau) GetConfig() *refractor.GameConfig {
+	return g.config
 }
 
 // GetWarnCommand returns an empty string since Mordhau does not have a warn command
@@ -37,4 +49,8 @@ func (g *mordhau) GetKickCommand(args refractor.CommandArgs) string {
 // The following fields must be present on CommandArgs: PlayerID, Duration, Reason
 func (g *mordhau) GetBanCommand(args refractor.CommandArgs) string {
 	return fmt.Sprintf("Ban %s %d %s", args.PlayerID, args.Duration, args.Reason)
+}
+
+func (g *mordhau) GetPlayerListCommand() string {
+	return "PlayerList"
 }
