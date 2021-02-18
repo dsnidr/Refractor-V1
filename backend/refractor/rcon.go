34ac480b139566da8efc9eb41809b6d0e03841b5
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-02-18 16:24:17 -0500

    Backend Broadcast Subscribers

--- backend/refractor/rcon.go
+++ backend/refractor/rcon.go
@@ -1,6 +1,9 @@
 package refractor
 
-import rcon "github.com/sniddunc/mordhau-rcon"
+import (
+	rcon "github.com/sniddunc/mordhau-rcon"
+	"github.com/sniddunc/refractor/pkg/broadcast"
+)
 
 // RCONClient wraps around a mordhau-rcon Client and has an extra field containing the server
 type RCONClient struct {
@@ -8,8 +11,12 @@ type RCONClient struct {
 	*rcon.Client
 }
 
+type BroadcastSubscriber func(fields broadcast.Fields, serverID int64)
+
 type RCONService interface {
 	CreateClient(*Server) error
 	GetClients() map[int64]*RCONClient
 	DeleteClient(serverID int64)
+	SubscribeJoin(subscriber BroadcastSubscriber)
+	SubscribeQuit(subscriber BroadcastSubscriber)
 }
