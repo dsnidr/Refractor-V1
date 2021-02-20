00dbba5af07921990fdee048977591f80c8441d5
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-02-20 15:48:50 -0500

    Backend Websocket Client Read Logic

--- backend/pkg/websocket/client.go
+++ backend/pkg/websocket/client.go
@@ -1,12 +1,20 @@
 package websocket
 
-import "net"
+import (
+	"encoding/json"
+	"github.com/gobwas/ws/wsutil"
+	"github.com/sniddunc/refractor/pkg/log"
+	"github.com/sniddunc/refractor/refractor"
+	"io"
+	"net"
+)
 
 type Client struct {
 	ID     int64
 	UserID int64
 	Conn   net.Conn
 	Pool   *Pool
+	log    log.Logger
 }
 
 var nextClientID int64 = 1
@@ -22,4 +30,31 @@ func NewClient(userID int64, conn net.Conn, pool *Pool) *Client {
 	nextClientID++
 
 	return client
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregister <- c
+		_ = c.Conn.Close()
+	}()
+
+	for {
+		msgBytes, _, err := wsutil.ReadClientData(c.Conn)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+
+			c.log.Warn("Could not read message from client %d. Error: %v\n", c.UserID, err)
+			continue
+		}
+
+		var msg *refractor.WebsocketMessage
+		if err = json.Unmarshal(msgBytes, &msg); err != nil {
+			c.log.Error("Could not unmarshal message: %v Error: %v\n", string(msgBytes), err)
+			continue
+		}
+
+		c.log.Info("Message received from client ID %d: %v\n", c.ID, msg)
+	}
 }
