1c5bbab49903687791438bb1633cd27fdc8742e3
Sniddunc <sniddunc2@gmail.com>
2021-03-01 12:38:09 -0500

    Backend Edit Server Params

--- backend/internal/params/server.go
+++ backend/internal/params/server.go
@@ -48,3 +48,44 @@ func (body *CreateServerParams) Validate() (bool, url.Values) {
 
 	return len(errors) == 0, errors
 }
+
+type EditServerParams struct {
+	Name         string `form:"name"`
+	Address      string `form:"address"`
+	RCONPort     string `form:"rconPort"`
+	RCONPassword string `form:"rconPassword"`
+}
+
+func (body *EditServerParams) Validate() (bool, url.Values) {
+	errors := url.Values{}
+
+	if body.Name != "" {
+		if len(body.Name) < config.ServerNameMinLen || len(body.Name) > config.ServerNameMaxLen {
+			errors.Set("name", fmt.Sprintf("Server name must be between %d and %d characters in length",
+				config.ServerNameMinLen, config.ServerNameMaxLen))
+		}
+	}
+
+	if body.Address != "" {
+		if net.ParseIP(body.Address) == nil {
+			errors.Set("address", "The provided server address was not a valid IP address")
+		}
+	}
+
+	if body.RCONPort != "" {
+		// Since port numbers are 16 bit ints, we can check if the provided port is valid by
+		// trying to parse it to an int16.
+		if _, err := strconv.ParseUint(body.RCONPort, 10, 16); err != nil {
+			errors.Set("rconPort", "The provided RCON port was not a valid port number")
+		}
+	}
+
+	if body.RCONPassword != "" {
+		if len(body.RCONPassword) < config.ServerPasswordMinLen || len(body.RCONPassword) > config.ServerPasswordMaxLen {
+			errors.Set("rconPassword", fmt.Sprintf("RCON passwords must be between %d and %d characters in length",
+				config.ServerPasswordMinLen, config.ServerPasswordMaxLen))
+		}
+	}
+
+	return len(errors) == 0, errors
+}
