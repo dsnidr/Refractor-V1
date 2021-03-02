53129a78a6faa84935d68a644e86c9d8b48427d0
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-03-02 15:34:58 -0500

    Frontend Select Component

---
+++ frontend/src/components/Select.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
+
+const Error = styled.div`
+	${(props) => css`
+		color: ${props.theme.colorDanger};
+		font-size: 1.2rem;
+		height: ${props.theme.errorHeight};
+		padding: 0.5rem;
+	`}
+`;
+
+const StyledSelect = styled.select`
+	${(props) => css`
+		width: 100%;
+		height: 100%;
+		background: ${props.theme.textInput.fillInBackground
+			? props.theme.colorBorderPrimary
+			: 'none'};
+		border: 1px solid ${props.theme.colorBorderPrimary};
+		border-radius: ${props.theme.borderRadiusNormal};
+		font-size: 1.6rem;
+		color: ${props.theme.colorTextPrimary};
+		padding-left: 1rem;
+
+		-webkit-appearance: none;
+		-moz-appearance: none;
+	`}
+`;
+
+class Select extends Component {
+	render() {
+		const { props } = this;
+
+		return (
+			<div>
+				<StyledSelect name={props.name} onChange={props.onChange}>
+					{this.props.children}
+				</StyledSelect>
+				<Error>{props.error ? props.error : null}</Error>
+			</div>
+		);
+	}
+}
+
+export default Select;
