993c92dbad27f7d411fc2ef986a3dd0e55bc2cbf
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-04-03 18:46:00 -0400

    Hide games with no servers in sidebar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import './index.scss';
+import './index.css';
 import { history, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { setInterceptors } from './utils/tokenUtils';
