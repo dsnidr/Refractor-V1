83f53b0493390cee2fbb849106d76c3059c95894
Sniddunc <sniddunc2@gmail.com>
2021-04-27 14:09:45 -0400

    Backend GPL Copyright Notices

--- backend/pkg/websocket/client.go
+++ backend/pkg/websocket/client.go
@@ -1,3 +1,20 @@
+/*
+This file is part of Refractor.
+
+Refractor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package websocket
 
 import (
@@ -38,9 +55,9 @@ func NewClient(userID int64, conn net.Conn, pool *Pool, log log.Logger, chatSend
 }
 
 type SendChatBody struct {
-	ServerID   int64 `json:"serverId"`
-	UserID     int64
-	Message    string `json:"message"`
+	ServerID int64 `json:"serverId"`
+	UserID   int64
+	Message  string `json:"message"`
 }
 
 func (c *Client) Read() {
