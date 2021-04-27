83349647cabd9cb9f2035863a04fd7217c324bbf
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-04-27 14:09:45 -0400

    Backend GPL Copyright Notices

--- backend/internal/websocket/service.go
+++ backend/internal/websocket/service.go
@@ -1,3 +1,20 @@
+/*
+This file is part of Refractor.
+
+Refractor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package websocket
 
 import (
@@ -46,9 +63,9 @@ func (s *websocketService) sendChatHandler(msgBody *websocket.SendChatBody) {
 	}
 
 	transformed := &refractor.ChatSendBody{
-		ServerID: msgBody.ServerID,
-		Message:  msgBody.Message,
-		Sender:   user.Username,
+		ServerID:   msgBody.ServerID,
+		Message:    msgBody.Message,
+		Sender:     user.Username,
 		SentByUser: true,
 	}
 
--- backend/refractor/user.go
+++ backend/refractor/user.go
@@ -1,3 +1,20 @@
+/*
+This file is part of Refractor.
+
+Refractor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package refractor
 
 import (
@@ -10,7 +27,7 @@ type User struct {
 	Email               string `json:"email"`
 	Username            string `json:"username"`
 	Password            string `json:"-"`
-	Permissions         int64 `json:"permissions"`
+	Permissions         int64  `json:"permissions"`
 	Activated           bool   `json:"activated"`
 	NeedsPasswordChange bool   `json:"needsPasswordChange"`
 }
@@ -20,7 +37,7 @@ type UserInfo struct {
 	Email               string `json:"email"`
 	Username            string `json:"username"`
 	Activated           bool   `json:"activated"`
-	Permissions         int64 `json:"permissions"`
+	Permissions         int64  `json:"permissions"`
 	NeedsPasswordChange bool   `json:"needsPasswordChange"`
 }
 
